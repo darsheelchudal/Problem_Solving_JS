a18ce5e41455c2a5b940fd0ba5a7c7cd0ba818b7
Darsheel Chudal <darsheelchudal11@gmail.com>
2025-09-10 22:32:23 +0545

    arr problem 1

---
+++ very_easy_questions/arrays/01.js
@@ -0,0 +1,13 @@
+// Create an array of 5 numbers and print it.
+
+// Algorithm
+// 1. Take an array and hold it in a variable
+// 2. Print the array
+
+// Pseudo-code
+// arrNumbers = [arr1,arr2,arr3,arr4,arr5]
+// Print arrNumbers
+
+// Source code
+const arrNumbers = [10, 20, 30, 40, 50];
+console.log(arrNumbers);
