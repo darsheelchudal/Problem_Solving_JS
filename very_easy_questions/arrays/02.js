0f0aa141969ac4d0d25af5d8b96a9002ec18f4b0
Darsheel Chudal <darsheelchudal11@gmail.com>
2025-09-11 21:24:38 +0545

    problem 2 solved

---
+++ very_easy_questions/arrays/02.js
@@ -0,0 +1,13 @@
+// Print the first element of an array.
+
+// Algorithm
+// 1. Take an array and hold it in a variable
+// 2. Print the first element of array using 0 index
+
+// Pseudocode
+// array = [1,2,3,4,5]
+// print arr[0]
+
+// Source code
+const arr = [1, 2, 3, 4, 5];
+console.log(arr[0]);
