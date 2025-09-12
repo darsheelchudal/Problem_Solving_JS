557c71d48f1ce75ed62295b4545f9ebdfedd08ae
Darsheel Chudal <darsheelchudal11@gmail.com>
2025-09-12 21:52:16 +0545

    question 3

---
+++ very_easy_questions/arrays/03.js
@@ -0,0 +1,13 @@
+// Print the last element of an array.
+
+// Algorithm
+// 1. Take an array and hold it in a variable
+// 2. Print the last elemeent of array
+
+// Pseudo-code
+// array = [1,3,4,5,6]
+// array[array.length-1]
+
+// Source code
+const myArr = [1, 3, 4, 65, 7];
+console.log(myArr[myArr.length - 1]);
